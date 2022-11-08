f2d424af3c0bbcf4042d5c7328ead8c90e59c485
Maruf Kabir Refat <53111498+Maruf-Kabir-Refat3666@users.noreply.github.com>
2022-11-08 15:43:13 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function show(btn, classifier,name) {
+  // black out the other buttons in the menu bar
+  var tabs = document.getElementsByClassName("w3-bar-item");
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("w3-grey");
+    tabs[i].classList.add("w3-black");
+  }
+  // colour this button
+  btn.classList.remove("w3-black");
+  btn.classList.add("w3-grey");
+  
+  // hide all the elements in the class
+  var x = document.getElementsByClassName(classifier);
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  // show the element identified
+  var panel = document.getElementById(name);
+  panel.style.display = "block";
+  
+  // refresh the iframe
+  var iframe = panel.getElementsByTagName("iframe");
+  iframe[0].src = iframe[0].src;
+}
+
+// Request user geolocation and callback with lat, lon
+function getLocation(fun) {
+    var is_safari = navigator.userAgent.toLowerCase().indexOf('safari/') > -1;
+    var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome/') > -1;
+    console.log(is_safari, is_chrome);
+    if (navigator.geolocation && (!is_safari || is_chrome)) {
+        navigator.geolocation.getCurrentPosition(function(loc) {
+          fun(loc.coords.latitude, loc.coords.longitude);
+        })
+    }
+    else {
+        alert("Geolocation is not supported by this browser.");
+        // location defaults to central Bristol
+        fun(51.47919, -2.59567);
+    }
+}
+
+// Load map with lat, lon query string
+function loadMap(lat,lon) {
+    location.href="map.html?lat="+lat+"&lon="+lon;
+}
